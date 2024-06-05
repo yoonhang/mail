90eccb164331fdfc64d260e0b7be159b595824cc
yoonhang <com717942@gmail.com>
2024-06-05 12:33:41 +0900

    first..

---
+++ back/src/main/java/com/rental/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+//@Controller
+//public class HomeController {
+//
+//        @GetMapping(value="/")
+//        public String home(Model model) {
+//            model.addAttribute("test", "test");
+//            return "index";
+//        }
+//
+//}
